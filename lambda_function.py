0145f633bd76be51785ba118ca3f4db5da77aba3
Eliza Augusta de Carvalho Neta <79981745+ElizaAugusta4@users.noreply.github.com>
2025-01-14 20:20:38 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,15 @@
-import boto3
-import datetime
+from flask import Flask, jsonify, request
 
-def lambda_handler(event, context):
-    bucket_name = "imagens-2025"
-    retention_days = 30  # Quantidade de dias antes de excluir o arquivo
-    
-    s3 = boto3.client('s3')
-    objects = s3.list_objects_v2(Bucket=bucket_name)
-    
-    if 'Contents' in objects:
-        for obj in objects['Contents']:
-            last_modified = obj['LastModified']
-            file_age = (datetime.datetime.now(datetime.timezone.utc) - last_modified).days
-            
-            if file_age > retention_days:
-                s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
-                print(f"Deleted {obj['Key']} (age: {file_age} days)")
-    else:
-        print("No objects to clean.")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to Flask API on AWS Lambda!"})
+
+@app.route("/echo", methods=["POST"])
+def echo():
+    data = request.get_json()
+    return jsonify({"echo": data})
+
+if __name__ == "__main__":
+    app.run(debug=True)
